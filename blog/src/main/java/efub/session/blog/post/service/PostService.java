d21252697c64732255c34f6c5396a54436179450
YEJI <yeji.lee.kim@gmail.com>
2023-04-14 00:16:13 +0900

    feat: removePost 예외 구체화 게시글 삭제에 실패했을 때, 파라미터로 넘겨준 postId와 accountId 중 어떤 것이 잘못 되었는지 구체적으로 메세지 작성

--- blog/src/main/java/efub/session/blog/post/service/PostService.java
+++ blog/src/main/java/efub/session/blog/post/service/PostService.java
@@ -27,7 +27,7 @@ public class PostService {
     public Post addPost(PostRequestDto requestDto) {
         Account writer = accountRepository.findById(requestDto.getAccountId())
                 .orElseThrow(()-> new IllegalArgumentException("존재하지 않는 계정입니다."));
-        // wrapper 클래스인 Account를 확인해줘야 함 -> orElseThorw로 예외 처리
+        // wrapper 클래스인 Account를 확인해줘야 함 -> orElseThrow로 예외 처리
 
         // db에 저장된 post를 반환
         return postRepository.save(
@@ -52,12 +52,23 @@ public class PostService {
     }
 
     public void removePost(Long postId, Long accountId) {
+        /*
         Post post = postRepository.findByPostIdAndWriter_AccountId(postId, accountId)
                 .orElseThrow(()->new IllegalArgumentException("잘못된 접근입니다."));
+        postRepository.delete(post);
+        */
+
         // findByPostIdAndWriter_AccountId -> postId, accountId 중 어떤 것이 잘못된 것인지 구체화해서 메소드 새로 작성해보기
         // 메소드를 쪼개면 메세지도 구체적으로 적을 수 있다.
+        Post post1 = postRepository.findById(postId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 게시글입니다."));    // 먼저 postId로 게시글을 찾는다.
 
-        postRepository.delete(post);
+        if(post1.getWriter().getAccountId() == accountId)
+            postRepository.delete(post1);    // 게시글이 존재하고 작성자가 일치하면, 글을 삭제한다.
+        else
+            throw new IllegalArgumentException("해당 글의 작성자가 아닙니다.");    // 게시글은 존재하나 작성자가 일치하지 않으면, 예외를 발생시킨다.
+
+
     }
 
 
